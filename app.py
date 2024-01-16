cbf72098cf39c82948b8444a7379add69e1ea66a
Darriecha <148508782+Darriecha@users.noreply.github.com>
2024-01-15 19:48:43 -0600

    Archivo de descarga de avatar

---
+++ app.py
@@ -0,0 +1,27 @@
+import requests
+
+BASE_URL = 'https://api.github.com/'
+
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+def download_avatar_user(avatar_url):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        # Download image
+        response_content = response.content
+        filename = 'avatar.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
+
+selected_user = get_github_user('Darriecha')
+print(download_avatar_user(selected_user.get('avatar_url')))
